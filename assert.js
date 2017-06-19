cc4d5615a2e8e99957f5e31578464477a2b8b547
Toby Winter <toby.hw@live.co.uk>
2017-06-19 16:43:37 +0100

    Fixes scr paths in index. Creates isEqual"

--- assert.js
+++ assert.js
@@ -1,16 +1,23 @@
 function Assert() {
   this.isTrue = function(assertion) {
     if (!assertion) {
-      throw new Error("Expected" + assertion + " to be True");
+      throw new Error("Expected " + assertion + " to be True.");
     }
     return (assertion + " is true");
   };
 
   this.isFalse = function(assertion) {
     if (assertion) {
-      throw new Error("Expected" + assertion + " to be False");
+      throw new Error("Expected " + assertion + " to be False.");
     }
     return (assertion + " is false");
+  };
+
+  this.isEqual = function(itemOne, itemTwo) {
+    if (itemOne !== itemTwo ) {
+      throw new Error("Expected " + itemOne + " to be Equal to " + itemTwo + "." );
+    }
+    return (itemOne + " is Equal to " + itemTwo);
   };
 }
 
