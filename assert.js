3affb094c58d412536ceab5e202d8122356f68a7
Toby Winter <toby.hw@live.co.uk>
2017-06-19 16:22:02 +0100

    Rewrite Assert function and adds isFalse function

--- assert.js
+++ assert.js
@@ -1,8 +1,17 @@
-var assert = {
-  isTrue: function(assertion) {
+function Assert() {
+  this.isTrue = function(assertion) {
     if (!assertion) {
-      throw new Error("Expected False to be True");
+      throw new Error("Expected" + assertion + " to be True");
     }
-    return (assertion + " is true")
-  }
-};
+    return (assertion + " is true");
+  };
+
+  this.isFalse = function(assertion) {
+    if (assertion) {
+      throw new Error("Expected" + assertion + " to be False");
+    }
+    return (assertion + " is false");
+  };
+}
+
+var assert = new Assert();
