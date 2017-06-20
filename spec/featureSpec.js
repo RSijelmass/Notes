56331f06bf2749c50965c3560a7c0ad7d9b974e3
RSijelmass <R.Sijelmass@gmail.com>
2017-06-20 16:24:43 +0100

    Experiment with bubble functions: not yet completed, doesn't read other <li> functions

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -10,6 +10,16 @@
     assert.hasContent('notes', message);
   };
 
+	/*function expandNote() {
+		var message = "Another Message, full of beary text. So much hair. Hair everywhere.";
+		var messageShort = message.slice(0, 20);
+
+    document.getElementById('new-note-text').value = message;
+    document.getElementById('add-note').click();
+		
+		
+	};*/
+
   function runFeatureTests() {
     createNewNote();
     console.log('Feature Tests Passed | Team BearFoot!');
--- views/interface.js
+++ views/interface.js
@@ -1,11 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+	//clickNotes();
+
 	document.getElementById('add-note').onclick = function() {
 		var newNote = new Note()
 		addTextToNote(newNote);
 		addChildParentRelation(newNote)
 		document.getElementById('new-note-text').value = '';
 	};
+
+	clickNotes = function() {
+		var notes = document.getElementsByTagName('ul');
+		console.log(notes);
+
+		function capture() {
+			console.log('capture: ' + this.firstChild.nodeValue.trim());
+			console.log(notes)
+		};
+
+		function bubble() {
+			console.log('bubble :' + this.firstChild.nodeValue.trim());
+		};
+		
+		for(var i=0; i < notes.length; i++) {
+			notes[i].addEventListener('click', capture, true);
+			notes[i].addEventListener('click', bubble, false);
+		};
+	};
+
+	clickNotes();
 
 	addTextToNote = function(note) {
 		var noteText = document.getElementById('new-note-text').value
