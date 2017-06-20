60aa34fb28a51a83c6006e671098ab1333fc31bb
RSijelmass <R.Sijelmass@gmail.com>
2017-06-20 12:05:12 +0100

    get note text from textarea - gave textarea id instead of name - update hardcoded 'Test Note' - create note every time button is clicked

--- views/interface.js
+++ views/interface.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	document.getElementById('add-note').onclick = function() {
+		var newNote = new Note()
+		var noteText = document.getElementById('new-note-text').value
+
+		newNote.text = noteText;
+
 		var notesList = document.getElementById('notes');
 		var note = document.createElement('li');
-		note.innerHTML = 'Test Note';
+		note.innerHTML = newNote.text; 
 
 		notesList.appendChild(note);
 	};
