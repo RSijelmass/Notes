6c8f24858e132d712c859ffbed8cc4ddd826dbd7
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-20 12:21:38 +0100

    Refactors newNote to a separate function addTextToNote

--- views/interface.js
+++ views/interface.js
@@ -2,15 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	document.getElementById('add-note').onclick = function() {
 		var newNote = new Note()
-		var noteText = document.getElementById('new-note-text').value
-
-		newNote.text = noteText;
-
+		addTextToNote(newNote);
 		var notesList = document.getElementById('notes');
 		var note = document.createElement('li');
-		note.innerHTML = newNote.text; 
+		note.innerHTML = newNote.text;
 
 		notesList.appendChild(note);
+	};
+
+	addTextToNote = function(note) {
+		var noteText = document.getElementById('new-note-text').value
+		note.text = noteText;
 	};
 
 });
